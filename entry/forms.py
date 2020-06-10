08fdf0f45065822f3d628a17716322dea3075df4
Israel Abraham <israelvictory87@gmail.com>
2020-06-10 12:15:04 +0100

    Modified forms.py file. #Added the placehlder attribute

--- entry/forms.py
+++ entry/forms.py
@@ -5,5 +5,19 @@ from .models import Entry
 class EntryForm(forms.ModelForm):
 
     class Meta:
-        model = Entry
-        fields = ('name', 'title', 'content')
+        model   = Entry
+        fields  = ('name', 'title', 'content')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Your name..'
+            }
+        ),
+            'title': forms.TextInput(attrs={
+                'placeholder': 'Your title..'
+            }
+        ),
+            'content': forms.Textarea(attrs={
+                'placeholder': "What's on your mind?"
+            }
+        ),
+    }
